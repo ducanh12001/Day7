2bbfe897f85bd640a94359fe200cac7949ce0ed8
ducanh12001 <70888594+ducanh12001@users.noreply.github.com>
2022-07-13 09:08:59 +0700

    o

--- Day7/TodoList/src/index.js
+++ Day7/TodoList/src/index.js
@@ -1,10 +1,51 @@
-import _ from 'lodash';
-
-function component() {
-  const element = document.createElement('div');
-  // Lodash, now imported by this script
-  //element.innerHTML = _.join(['Hello', 'webpack'], ' ')
-  return element;
+var myNodelist = document.getElementsByTagName("LI");
+var i;
+for (i = 0; i < myNodelist.length; i++) {
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  myNodelist[i].appendChild(span);
 }
 
-document.body.appendChild(component());
+var close = document.getElementsByClassName("close");
+var i;
+for (i = 0; i < close.length; i++) {
+  close[i].onclick = function () {
+    var div = this.parentElement;
+    div.style.display = "none";
+  }
+}
+
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+  if (ev.target.tagName === 'LI') {
+    ev.target.classList.toggle('checked');
+  }
+}, false);
+
+function newElement() {
+  var li = document.createElement("li");
+  var inputValue = document.getElementById("task").value;
+  var t = document.createTextNode(inputValue);
+  li.appendChild(t);
+  if (inputValue === '') {
+    alert("Write something!");
+  } else {
+    document.getElementById("list").appendChild(li);
+  }
+  document.getElementById("task").value = "";
+
+  var span = document.createElement("SPAN");
+  var txt = document.createTextNode("\u00D7");
+  span.className = "close";
+  span.appendChild(txt);
+  li.appendChild(span);
+
+  for (i = 0; i < close.length; i++) {
+    close[i].onclick = function () {
+      var div = this.parentElement;
+      div.style.display = "none";
+    }
+  }
+}
